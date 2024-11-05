db986d5b74cb79a4a6c035bdb55115a9dfa2f0ab
shray-092 <shraypatney2005@gmail.com>
2024-11-05 15:03:36 +0530

    work

---
+++ Day7/script.js
@@ -0,0 +1,24 @@
+async function fetchdata() {
+    try {
+        let response = await fetch("https://api.github.com/users?per_page=10");
+        let data = await response.json();
+        
+        data.forEach(element => {
+            //console.log(`node id: ${element.node_id}, html url: ${element.html_url}`);
+            let h1=document.createElement('h1');
+            h1.textContent=element.node_id;
+            let a=document.createElement('a');
+            a.textContent=element.node_id;
+            a.setAttribute("href",element.html_url);
+            let top=document.getElementById("top");
+            top.appendChild(h1);
+            top.appendChild(a);
+
+
+        });
+    } catch (error) {
+        console.log(error);json
+    }
+}
+
+
